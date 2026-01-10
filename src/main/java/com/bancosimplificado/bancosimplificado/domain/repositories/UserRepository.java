515062ffbc242618aecf7afdb25a94283b33f4b3
Igor bruno <igordf2502@gmail.com>
2026-01-10 03:30:11 -0300

    feat(user): criar UserRepository

---
+++ src/main/java/com/bancosimplificado/bancosimplificado/domain/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bancosimplificado.bancosimplificado.domain.repositories;
+
+import com.bancosimplificado.bancosimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserByid(long id);
+}
