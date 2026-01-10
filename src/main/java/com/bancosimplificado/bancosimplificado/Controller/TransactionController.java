55095a69e14b022bfc685b0a84ff611bb8abae8d
Igor bruno <igordf2502@gmail.com>
2026-01-10 06:21:28 -0300

    feat: Adição de swagger

--- src/main/java/com/bancosimplificado/bancosimplificado/Controller/TransactionController.java
+++ src/main/java/com/bancosimplificado/bancosimplificado/Controller/TransactionController.java
@@ -10,14 +10,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/transactions")
+@Tag(name = "Transactions", description = "Operações de transferência entre usuários")
 public class TransactionController {
+
     @Autowired
     private TransactionService transactionService;
 
     @PostMapping
+    @Operation(summary = "Criar transação", description = "Realiza uma transferência entre usuários")
     public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDto transaction) throws Exception {
         Transaction newTransaction = this.transactionService.createTransaction(transaction);
         return new ResponseEntity<>(newTransaction, HttpStatus.OK);
--- src/main/java/com/bancosimplificado/bancosimplificado/Controller/UserController.java
+++ src/main/java/com/bancosimplificado/bancosimplificado/Controller/UserController.java
@@ -7,23 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
 
-@RestController()
+@RestController
 @RequestMapping("/users")
+@Tag(name = "Users", description = "Operações relacionadas a usuários")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping
+    @Operation(summary = "Criar usuário", description = "Cria um novo usuário no sistema")
     public ResponseEntity<User> createUser(@RequestBody UserDto user){
         User newUser = userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Listar usuários", description = "Retorna todos os usuários cadastrados")
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> users = this.userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
