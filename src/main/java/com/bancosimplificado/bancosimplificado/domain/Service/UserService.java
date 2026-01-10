8f86de0b726bbf407e7a10ca4f6ebfb0d2977b51
Igor bruno <igordf2502@gmail.com>
2026-01-10 03:50:29 -0300

    feat: criar UserService

---
+++ src/main/java/com/bancosimplificado/bancosimplificado/domain/Service/UserService.java
@@ -0,0 +1,30 @@
+package com.bancosimplificado.bancosimplificado.domain.Service;
+
+import com.bancosimplificado.bancosimplificado.domain.repositories.UserRepository;
+import com.bancosimplificado.bancosimplificado.domain.user.User;
+import com.bancosimplificado.bancosimplificado.domain.user.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar transação");
+        }
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findById(id)
+                .orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+}
--- src/main/java/com/bancosimplificado/bancosimplificado/domain/repositories/UserRepository.java
+++ src/main/java/com/bancosimplificado/bancosimplificado/domain/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
-    Optional<User> findUserByid(long id);
+    Optional<User> findUserById(long id);
 }
